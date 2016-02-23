f12430ee56666182f5a2f235008c277519a9416b
Aaron Fabiani <aaron.fabiani@bell.ca>
2016-02-23 12:52:42 -0500

    initial commit

---
+++ java-beginners-edition/src/Example.java
@@ -0,0 +1,9 @@
+/*
+	This is a simple java program
+*/
+class Example {
+	// begins with call to main()
+	public static void main(String args[]) {
+		System.out.println("java example");
+	}
+}
---
+++ java-beginners-edition/src/Example2.java
@@ -0,0 +1,14 @@
+/* demonstrates variables
+*/
+
+class Example2 {
+	public static void main(String args[]) {
+		int var1, var2;
+		
+		var1 = 1024;
+		System.out.println("var1 contains " + var1);
+		var2 = var1 /2;
+		System.out.print("var2 contains var1 / 2: ");
+		System.out.println(var2);
+	}
+}
