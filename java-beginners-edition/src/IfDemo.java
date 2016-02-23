076f5d7a8371e895fdf0f77275fbb74f68547a2f
Aaron Fabiani <aaron.fabiani@bell.ca>
2016-02-23 13:11:36 -0500

    new if statement

---
+++ java-beginners-edition/src/IfDemo.java
@@ -0,0 +1,22 @@
+class IfDemo{
+	public static void main(String args[]) {
+		int a, b, c;
+		
+		a = 2;
+		b = 3;
+		
+		if (a < b) System.out.println(" a is less than  b");
+		if (a == b) System.out.println("you won't see this");
+		
+		System.out.println();
+		
+		c = a - b; // c contains -1
+		
+		System.out.println("c contains -1");
+		
+		if(c >= 0) System.out.println("c is not negative");
+		if(c < 0) System.out.println("c is negative");
+		
+		
+	}
+}
