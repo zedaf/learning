689a9d8ea2b844f9531424e17a7b3e0c5ff11fba
Aaron Fabiani <aaron.fabiani@bell.ca>
2016-02-23 13:58:41 -0500

    some more stuff

---
+++ java-beginners-edition/src/BlockDemo.java
@@ -0,0 +1,16 @@
+/* Demo a block of code */
+
+class BlockDemo {
+	public static void main(String args[]) {
+		double i, j, d;
+		
+		i = 5;
+		j = 10;
+		
+		if(i != 0) {
+			System.out.println("i does not equal zero");
+			d = j / i;
+			System.out.println("j / i is " + d);
+		}
+	}
+}
